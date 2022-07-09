e67f989843c02a3ee40e52f2d1e0932bc859ee30
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-09 13:40:57 -0300

    Change in activity

--- app/src/main/java/com/touchetime/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/touchetime/presentation/ui/MainActivity.kt
@@ -2,14 +2,30 @@ package com.touchetime.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.touchetime.databinding.ActivityMainBinding
+import com.touchetime.presentation.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        showHomeFragment()
+    }
+
+    private fun showFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(viewBinding.container.id, fragment, tag)
+            .commit()
+    }
+
+    private fun showHomeFragment() {
+        showFragment(HomeFragment.netInstance(), HomeFragment::class.java.name)
     }
 }
