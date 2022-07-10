2d2f21e7a29ed2c7eb9bf3965b9d1794cba8e4ba
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-10 13:29:39 -0300

    Scoreboard View creation

---
+++ app/src/main/java/com/touchetime/presentation/common/ScoreBoardView.kt
@@ -0,0 +1,87 @@
+package com.touchetime.presentation.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.touchetime.R
+import com.touchetime.databinding.ScoreboardViewBinding
+
+class ScoreBoardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val viewBinding = ScoreboardViewBinding.inflate(
+        LayoutInflater.from(context), this, true
+    )
+
+    fun setupColor(@StringRes color: Int) {
+        viewBinding.apply {
+            if (color == R.string.blue) {
+                buildScoreBoard(
+                    R.drawable.background_scoreboard_blue,
+                    R.drawable.background_score_blue,
+                    R.drawable.background_button_blue_touche
+                )
+            } else {
+                buildScoreBoard(
+                    R.drawable.background_scoreboard_red,
+                    R.drawable.background_score_red,
+                    R.drawable.background_button_red_touche
+                )
+            }
+        }
+    }
+
+    fun setupTouche(callback: () -> Unit) {
+        viewBinding.touche.setOnClickListener {
+            callback()
+        }
+    }
+
+    fun setupAddScore(callback: () -> Unit) {
+        viewBinding.addScore.setOnClickListener {
+            callback()
+        }
+    }
+
+    fun setupRemoveScore(callback: () -> Unit) {
+        viewBinding.removeScore.setOnClickListener {
+            callback()
+        }
+    }
+
+    fun setupFoul(callback: () -> Unit) {
+        viewBinding.foulContainer.setOnClickListener {
+            callback()
+        }
+    }
+
+    private fun buildScoreBoard(
+        @DrawableRes background: Int,
+        @DrawableRes backgroundScore: Int,
+        @DrawableRes backgroundTouche: Int
+    ) {
+        viewBinding.apply {
+            this.container.background =
+                ContextCompat.getDrawable(context, background)
+            this.addScore.background =
+                ContextCompat.getDrawable(
+                    context,
+                    backgroundScore
+                )
+            this.removeScore.background =
+                ContextCompat.getDrawable(
+                    context,
+                    backgroundScore
+                )
+            this.touche.background =
+                ContextCompat.getDrawable(context, backgroundTouche)
+        }
+    }
+}
