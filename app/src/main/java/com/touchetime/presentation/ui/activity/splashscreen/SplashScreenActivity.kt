28a7a145627ce489936bcd6074dadfd01925c974
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-12 13:23:19 -0300

    Update splash screen

--- app/src/main/java/com/touchetime/presentation/ui/activity/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/touchetime/presentation/ui/activity/splashscreen/SplashScreenActivity.kt
@@ -34,6 +34,6 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val OPEN_DELAY = 1500L
+        private const val OPEN_DELAY = 1000L
     }
 }
