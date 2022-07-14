cb6b2f1227c5a1909b1cd3164860717d98389780
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-14 10:07:48 -0300

    Update category

--- app/src/main/java/com/touchetime/presentation/ui/fragments/category/Category.kt
+++ app/src/main/java/com/touchetime/presentation/ui/fragments/category/Category.kt
@@ -6,10 +6,11 @@ import com.touchetime.presentation.model.ObjectToSelect
 object Category {
 
     private val listCategory: List<ObjectToSelect> = listOf(
-        ObjectToSelect(R.string.u13),
+        ObjectToSelect(R.string.childrens),
         ObjectToSelect(R.string.u15),
         ObjectToSelect(R.string.u17),
         ObjectToSelect(R.string.u20),
+        ObjectToSelect(R.string.u23),
         ObjectToSelect(R.string.senior),
     )
 
