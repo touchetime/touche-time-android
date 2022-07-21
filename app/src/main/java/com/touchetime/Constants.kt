fa6e65ef13433985d1fbe36774970c1bed905a9f
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-21 14:35:58 -0300

    Update time

--- app/src/main/java/com/touchetime/Constants.kt
+++ app/src/main/java/com/touchetime/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val DELAY = 1000L
     const val GRECO_ROMAN_SUPERIORITY = 8
     const val FREE_STYLE_SUPERIORITY = 10
-    const val TIME_ROUND_TREE_MINUTES = 5000L // 180000L
-    const val TIME_ROUND_TWO_MINUTES = 4000L // 100000L
-    const val TIME_INTERVAL = 3000L // 30000L
+    const val TIME_ROUND_TREE_MINUTES = 180000L
+    const val TIME_ROUND_TWO_MINUTES = 100000L
+    const val TIME_INTERVAL = 30000L
 }
