d04110a795853697d2e1962f7426f926febd1b3f
Wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-07-27 18:41:16 -0300

    Fight custom view creation

---
+++ app/src/main/java/com/touchetime/presentation/common/FightCustomView.kt
@@ -0,0 +1,29 @@
+package com.touchetime.presentation.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.touchetime.databinding.FightCustomViewBinding
+
+class FightCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val viewBinding = FightCustomViewBinding.inflate(
+        LayoutInflater.from(context),
+        this,
+        true
+    )
+
+    fun setupNameFight(value: String) {
+        viewBinding.title.text = value
+    }
+
+    fun setupScore(red: Int, blue: Int) {
+        viewBinding.scoreBlue.text = red.toString()
+        viewBinding.scoreRed.text = red.toString()
+    }
+}
